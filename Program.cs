0614f6e4417a363177e6fd7eb7779ce45207e1eb
GamoraKrut <arturgamurar1488@gmail.com>
2023-03-05 05:36:11 +0300

    done task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+string[] userArray;
+
+Console.WriteLine("Введите массив строк через пробел");
+string userString = Console.ReadLine();
+
+userArray = userString.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+var result = new string[userString.Length];
+int point = 0;
+
+foreach (var symbols in userArray)
+{
+    if (symbols.Length <= 3)
+    {
+        result[point] = symbols;
+        point++;
+    }
+}
+Console.Write(string.Join(", ", result, 0, point));
